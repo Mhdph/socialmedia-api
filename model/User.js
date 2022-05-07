19d11d65043dda4ac99cffc6e779fff6edc6962f
mahdi parivash <mahdiparivash@protonmail.com>
2022-05-07 13:22:50 +0430

    Update User Model

--- model/User.js
+++ model/User.js
@@ -4,7 +4,7 @@ const UserSchema = new mongoose.Schema(
   {
     username: {
       type: String,
-      required: true,
+      require: true,
       min: 3,
       max: 20,
       unique: true,
@@ -19,7 +19,6 @@ const UserSchema = new mongoose.Schema(
       type: String,
       required: true,
       min: 6,
-      max: 17,
     },
     profilePicture: {
       type: String,
@@ -33,13 +32,29 @@ const UserSchema = new mongoose.Schema(
       type: Array,
       default: [],
     },
-    following: {
+    followings: {
       type: Array,
       default: [],
     },
     isAdmin: {
       type: Boolean,
       default: false,
+    },
+    desc: {
+      type: String,
+      max: 50,
+    },
+    city: {
+      type: String,
+      max: 50,
+    },
+    from: {
+      type: String,
+      max: 50,
+    },
+    relationship: {
+      type: Number,
+      enum: [1, 2, 3],
     },
   },
   { timestamps: true }
