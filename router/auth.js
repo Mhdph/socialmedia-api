e17f047a7b772e9a6a7768f527502530d3dad28f
mahdi parivash <mahdiparivash@protonmail.com>
2022-05-07 13:23:55 +0430

    complate auth route

--- router/auth.js
+++ router/auth.js
@@ -1,41 +1,34 @@
 const router = require("express").Router();
 const User = require("../model/User");
-const bcrypt = require("bcrypt");
+const CryptoJS = require("crypto-js");
 
 //REGISTER
 router.post("/register", async (req, res) => {
-  //generate new password
-  const salt = await bcrypt.genSalt(10);
-  const hashedPassword = await bcrypt.hash(req.body.password, salt);
-
-  //create new user
   const newUser = new User({
     username: req.body.username,
     email: req.body.email,
-    password: hashedPassword,
+    password: CryptoJS.AES.encrypt(
+      req.body.password,
+      process.env.PASS_SEC
+    ).toString(),
   });
 
-  //save user and respond
-  const user = await newUser.save();
-  res.status(200).json(user);
+  const savedUser = await newUser.save();
+  res.status(201).json(savedUser);
 });
 
 //LOGIN
 router.post("/login", async (req, res) => {
-  try {
-    const user = await User.findOne({ email: req.body.email });
-    !user && res.status(404).json("user not found");
+  const user = await User.findOne({ email: req.body.email });
+  !user && res.status(401).json("Wrong password or username!");
 
-    const validPassword = await bcrypt.compare(
-      req.body.password,
-      user.password
-    );
-    !validPassword && res.status(400).json("wrong password");
+  const bytes = CryptoJS.AES.decrypt(user.password, process.env.PASS_SEC);
+  const originalPassword = bytes.toString(CryptoJS.enc.Utf8);
 
-    res.status(200).json(user);
-  } catch (err) {
-    res.status(500).json(err);
-  }
+  originalPassword !== req.body.password &&
+    res.status(401).json("Wrong password or username!");
+
+  res.status(200).json(user);
 });
 
 module.exports = router;
